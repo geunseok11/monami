ef8274f8d13169ef7ff402ee4cb763c253f670a3
geunseok11 <junggeunseok11@gmail.com>
2022-02-25 23:28:42 +0900

    ADD basketlist

--- com/monami/product/BucketCancelOkAction.java
+++ com/monami/product/BucketCancelOkAction.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.monami.action.Action;
 import com.monami.action.ActionTo;
 
-public class BucketCancelOkAction implements Action{
+public class BucketCancelOkAction implements Action {
 
 	@Override
 	public ActionTo execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
@@ -15,36 +15,33 @@ public class BucketCancelOkAction implements Action{
 		String prodidx = req.getParameter("prodidx");
 		String prodcnt = req.getParameter("prodcnt");
 		String keyword = prodidx + ":" + prodcnt;
-		
+
 		String bucket = "";
 		String value = "";
-		
+
 		ActionTo transfer = new ActionTo();
-		
-		if(cookie != null) {
+
+		if (cookie != null) {
 			Cookie[] cookies = req.getCookies();
-			
-			for(int i = 0; i < cookies.length; i++) {
-				if(cookies[i].getName().equals("bucket")) {
+
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("bucket")) {
 					bucket = cookies[i].getValue();
 					String[] prod = cookies[i].getValue().split("/");
-					for(int j = 0; j < prod.length; j++) {
-						if(!prod[i].equals(keyword)) {
+					for (int j = 0; j < prod.length; j++) {
+						if (!prod[i].equals(keyword)) {
 							value += prod[i];
 						}
 					}
 				}
 			}
 			Cookie changeCookie = new Cookie("bucket", value);
-			resp.addCookie(changeCookie);			
+			resp.addCookie(changeCookie);
 			transfer.setRedirect(false);
 			transfer.setPath("/geunseok/BucketList.pd");
 			return transfer;
 		}
-		
-		
-		
-		
+
 		return null;
 	}
 
---
+++ com/monami/product/BucketListAction.java
@@ -0,0 +1,51 @@
+package com.monami.app.product;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.monami.action.Action;
+import com.monami.action.ActionTo;
+import com.monami.app.product.dao.ProductDAO;
+import com.monami.app.product.dao.ProductDTO;
+
+public class BucketListAction implements Action {
+
+	@Override
+	public ActionTo execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		ProductDAO pdao = new ProductDAO();
+		String cookie = req.getHeader("Cookie");
+		String bucket = "";
+		int prod_idx = 0;
+
+		if (cookie != null) {
+			Cookie[] cookies = req.getCookies();
+			for (int i = 0; i < cookies.length; i += 2) {
+				if (cookies[i].getName().equals("bucket")) {
+					bucket = cookies[i].getValue();
+				}
+			}
+		}
+		String[] list = bucket.split("/");
+		ProductDTO product = new ProductDTO();
+		ArrayList<ProductDTO> basketlist = new ArrayList<ProductDTO>();
+		if (list != null) {
+			for (int i = 0; i < list.length; i++) {
+				product = pdao.getBasket(Integer.parseInt(list[i].substring(0, 1)));
+				basketlist.add(product);
+			}
+		}
+
+		req.setAttribute("basketlist", basketlist);
+
+		ActionTo transfer = new ActionTo();
+
+		transfer.setRedirect(false);
+		transfer.setPath("/geunseok/bucket.jsp");
+
+		return transfer;
+	}
+
+}
